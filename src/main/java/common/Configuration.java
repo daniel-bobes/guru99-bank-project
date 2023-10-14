7ef91bc7031c512eb9eaf76eefdff240b33654be
Daniel Bobes de Pedro <danibobesdepedro@gmail.com>
2023-10-14 18:50:10 +0200

    Test parametrizado para verificar el login

--- src/main/java/common/Configuration.java
+++ src/main/java/common/Configuration.java
@@ -4,4 +4,7 @@ public final class Configuration {
     public static final String BASE_URL = "https://demo.guru99.com/V4/";
     public static final String EXPECTED_USERID = "mngr532405";
     public static final String EXPECTED_PASSWORD = "ahYpagy";
+
+    public static final String HOME_PAGE_TITLE = "Guru99 Bank Home Page";
+    public static final String MANAGER_HOME_PAGE_TITLE = "Guru99 Bank Manager HomePage";
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,9 +1,15 @@
 import common.BaseClass;
 import common.Configuration;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,7 +26,7 @@ public class LoginTest extends BaseClass {
 
         WebElement passwordField = getDriver().findElement(By.name("password"));
         passwordField.sendKeys(Configuration.EXPECTED_PASSWORD);
-        assertEquals(Configuration.EXPECTED_PASSWORD, passwordField.getAttribute("value"), "Invalid password");
+        assertEquals(Configuration.EXPECTED_PASSWORD, passwordField.getAttribute("value"), "Abc123");
 
         // Another way: password.submit()
         getDriver().findElement(By.name("btnLogin")).click();
@@ -30,6 +36,37 @@ public class LoginTest extends BaseClass {
         String expectedHeadingText = "Manger Id : " + Configuration.EXPECTED_USERID;
         WebElement heading = getDriver().findElement(By.cssSelector("tr.heading3 > td"));
         assertEquals(expectedHeadingText, heading.getText(), "Invalid heading text");
+
+        assertEquals(Configuration.MANAGER_HOME_PAGE_TITLE, getDriver().getTitle(), "Invalid page title");
+    }
+
+    private static Stream<Arguments> provideArgumentForVerifyLogin() {
+        return Stream.of(
+                Arguments.of(Configuration.EXPECTED_USERID, Configuration.EXPECTED_PASSWORD,
+                        "valid", Configuration.MANAGER_HOME_PAGE_TITLE),
+                Arguments.of("TomSmith", Configuration.EXPECTED_PASSWORD,
+                        "invalid", Configuration.HOME_PAGE_TITLE),
+                Arguments.of(Configuration.EXPECTED_USERID, "Abc123",
+                        "invalid", Configuration.HOME_PAGE_TITLE),
+                Arguments.of("TomSmith", "invalid passowrd",
+                        "invalid", Configuration.HOME_PAGE_TITLE)
+        );
+    }
+
+    @DisplayName("Verifying login with both invalid and valid credentials")
+    @ParameterizedTest(name = "{index} Login with username '{0}' and password '{1}' should be {2} login")
+    @MethodSource("provideArgumentForVerifyLogin")
+    public void verifyLogin(String username, String password, String expectedLoginResult, String expectedPageTitle) {
+        getDriver().findElement(By.name("uid")).sendKeys(username);
+        getDriver().findElement(By.name("password")).sendKeys(password);
+        getDriver().findElement(By.name("btnLogin")).click();
+
+        if (expectedLoginResult.equals("invalid")) {
+            getWait().until(ExpectedConditions.alertIsPresent()).accept();
+            getWait().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h2.barone")));
+        }
+
+        assertEquals(expectedPageTitle, getDriver().getTitle(), "Invalid page title");
     }
 
 }
